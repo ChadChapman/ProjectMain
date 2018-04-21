d254af18dff4cf2c70179f25ebeedf412d2bbad2
Raymond Schooley <ravschoo@uw.edu>
2018-04-20 20:08:40 -0700

    Login sprint1 branch initial commit

--- app/src/main/java/tcss450/uw/edu/group2project/LoginFragment.java
+++ app/src/main/java/tcss450/uw/edu/group2project/LoginFragment.java
@@ -14,7 +14,6 @@ import android.view.ViewGroup;
  * Activities that contain this fragment must implement the
  * {@link LoginFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link LoginFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class LoginFragment extends Fragment {
