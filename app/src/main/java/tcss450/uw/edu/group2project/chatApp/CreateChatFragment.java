5134fbe9746033aa55089a8918b4a38a5cccd172
git_dave <ctchapman@gmail.com>
2018-05-15 19:06:09 -0700

    at a decision point of where to put the list of contacts, we will want them to be available to multiple frags and activities

--- app/src/main/java/tcss450/uw/edu/group2project/chatApp/CreateChatFragment.java
+++ app/src/main/java/tcss450/uw/edu/group2project/chatApp/CreateChatFragment.java
@@ -4,21 +4,34 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+
+import java.util.List;
 
 import tcss450.uw.edu.group2project.R;
+import tcss450.uw.edu.group2project.model.ChatContact;
+import tcss450.uw.edu.group2project.model.ContactFeedItem;
+import tcss450.uw.edu.group2project.model.FeedItem;
+import tcss450.uw.edu.group2project.utils.MyRecyclerViewAdapter;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link CreateChatFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- */
+
 public class CreateChatFragment extends Fragment {
+    private static final String TAG = "create_chat_fragment";
+    //private OnFragmentInteractionListener mListener;
+    private RecyclerView mRecyclerView;
+    private MyRecyclerViewAdapter adapter;
+    private ProgressBar progressBar;
+    //private List<ChatContact> mContactsList;
+    private List<ContactFeedItem> mContactFeedItemList;
+    private int mUserMemberID;
+    private String mUserMemberIDStr;
+    private Uri mContactsUri;
 
-    private OnFragmentInteractionListener mListener;
 
     public CreateChatFragment() {
         // Required empty public constructor
@@ -29,45 +42,54 @@ public class CreateChatFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_chat, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_create_chat, container, false);
+        
+
+
+        //param passed here may not work
+        RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.recycler_view);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return rootView;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
+//    // TODO: Rename method, update argument and hook method into UI event
+//    public void onButtonPressed(Uri uri) {
+//        if (mListener != null) {
+//            mListener.onFragmentInteraction(uri);
+//        }
+//    }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+//        if (context instanceof OnFragmentInteractionListener) {
+//            mListener = (OnFragmentInteractionListener) context;
+//        } else {
+//            throw new RuntimeException(context.toString()
+//                    + " must implement OnFragmentInteractionListener");
+//        }
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
+       // mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
+//    /**
+//     * This interface must be implemented by activities that contain this
+//     * fragment to allow an interaction in this fragment to be communicated
+//     * to the activity and potentially other fragments contained in that
+//     * activity.
+//     * <p>
+//     * See the Android Training lesson <a href=
+//     * "http://developer.android.com/training/basics/fragments/communicating.html"
+//     * >Communicating with Other Fragments</a> for more information.
+//     */
+//    public interface OnFragmentInteractionListener {
+//        // TODO: Update argument type and name
+//        void onFragmentInteraction(Uri uri);
+//    }
 }
